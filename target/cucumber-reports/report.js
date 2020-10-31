d0de922ac40581d3d5b60d221a3b82f46c163718
Vishwanath <Vishwanath@BLRPC12.e-softsys.local>
2020-10-30 23:38:58 -0400

    added report

--- target/cucumber-reports/report.js
+++ target/cucumber-reports/report.js
@@ -7,7 +7,7 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 4367599600,
+  "duration": 5505555800,
   "status": "passed"
 });
 formatter.background({
@@ -52,22 +52,23 @@ formatter.match({
   "location": "Mapping.user_is_on_the_login_page()"
 });
 formatter.result({
-  "duration": 3549535300,
+  "duration": 3719567600,
   "status": "passed"
 });
 formatter.match({
   "location": "Mapping.user_enters_the_Username_and_Password(DataTable)"
 });
 formatter.result({
-  "duration": 1250103800,
+  "duration": 1220214600,
   "status": "passed"
 });
 formatter.match({
   "location": "Mapping.login_will_be_successfull()"
 });
 formatter.result({
-  "duration": 1142002900,
-  "status": "passed"
+  "duration": 327781100,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\".ColToday1\"}\n  (Session info: chrome\u003d86.0.4240.111)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:25:48\u0027\nSystem info: host: \u0027BLRPC12\u0027, ip: \u0027192.168.0.12\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002714.0.2\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 86.0.4240.111, chrome: {chromedriverVersion: 85.0.4183.87 (cd6713ebf92fa..., userDataDir: C:\\Users\\VISHWA~1\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:51741}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 43a1a45d2ca23adda407fac426c1c265\n*** Element info: {Using\u003dclass name, value\u003dColToday1}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByClassName(RemoteWebDriver.java:412)\r\n\tat org.openqa.selenium.By$ByClassName.findElement(By.java:389)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat stepdefinitions.Mapping.login_will_be_successfull(Mapping.java:69)\r\n\tat ✽.Then login will be successfull.(F:/vishwa/Amar/Features/maps.feature:9)\r\n",
+  "status": "failed"
 });
 formatter.scenario({
   "line": 12,
@@ -237,18 +238,16 @@ formatter.match({
   "location": "Mapping.user_fills_all_the_details_regarding_the_time_entry(DataTable)"
 });
 formatter.result({
-  "duration": 48640491100,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "Mapping.send_the_mail_automatically(DataTable)"
 });
 formatter.result({
-  "duration": 2674754200,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
-  "duration": 4326575700,
+  "duration": 112933800,
   "status": "passed"
 });
 });
